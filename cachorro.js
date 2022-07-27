10245c26923c5554bd96e2fc4396c35d7821c27e
Luciana Rodrigues <lucyfierceit@gmail.com>
2022-07-27 12:15:49 -0300

    adicionando

---
+++ cachorro.js
@@ -0,0 +1,5 @@
+let cachorro = require ("./cao");
+console.log("O cachorro se chama " + cachorro.nome + "e tem " + cachorro.idade  + " anos")
+
+
+
---
+++ cinema.js
@@ -0,0 +1,33 @@
+/*let cinema = "CineHouse";
+
+let filmes = require ("./node_modules/filmes/index");
+console.log(filmes)
+*/
+//let filmes = require ("./database/catalogo.json");
+//console.log(filmes)
+
+//let filmes = require ("./database/catalogo.json");
+//let filmeStr = JSON.stringify(filmes); //para transformar em string usa-se stringify 
+//let filmeJS = JSON.parse (filmes); //para transformar em Json usa-se parse
+//console.log(filmeStr)
+
+function listarTodosOsFilmes 
+
+//let moment = require ("moment");
+//let data = moment().format ("MMM do YY");
+// console.log(data)
+
+/*
+let moduloNativo = require("http");
+let moduloInstalado = require("axios");
+let moduloProprio = require ("./minhafuncao")
+*/
+/*
+let fs = require ("fs");
+
+let dados = fs.readFileSync(__dirname + "/dados.txt", "utf-8");
+console.log(dados)
+
+*/
+
+
